5cc8f924951b16e262179a13105f0167ca3846f0
juan <juanvaldebenitovaldivia@outlook.es>
2020-11-28 10:37:38 -0300

    first commit

---
+++ precache-manifest.ffb46cde081fcf5abc9a9e21753f89cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d43ee01c5088c8c2f154636e27808d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf37db6a97701a399a73",
+    "url": "/static/css/main.edf874bb.chunk.css"
+  },
+  {
+    "revision": "89b99147dd0d59a58763",
+    "url": "/static/js/2.ec023188.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec023188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf37db6a97701a399a73",
+    "url": "/static/js/main.8f435d5e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
